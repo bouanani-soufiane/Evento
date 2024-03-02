36b93f0efa3634f5ca8c8a87a2c586d7a025e03e
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-03-02 05:17:57 +0100

    working on dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource("/dashboard/categories", \App\Http\Controllers\CategoryController::class);
+Route::resource("/dashboard/events", \App\Http\Controllers\EventController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
